04ce81738e9b58e28c5ad3f837c8ab6019eb54f3
Kent Ng <kent.ng201@gmail.com>
2024-10-17 07:09:35 +0800

    expose multi database API usage

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { setEnvironment } from '.';
 import { setRealtime } from '.';
+import { getMainDatabaseName, getPerformanceMode, setMainDatabaseName, setPerformanceMode } from './multi-database/MutliDatabaseConfig';
 
 export * from 'src/manager/DatabaseManager';
 export * from 'src/manager/RepoManager';
@@ -12,7 +13,11 @@ export * from 'src/relationships/RelationshipDecorator';
 export * from 'src/model/ModelDecorator';
 export * from 'src/helpers/Persistor';
 
-export const PocketDefault = {
+export const Pocketto = {
     setRealtime: setRealtime,
     setEnvironment: setEnvironment,
+    setPerformanceMode: setPerformanceMode,
+    getPerformanceMode: getPerformanceMode,
+    setMainDatabaseName: setMainDatabaseName,
+    getMainDatabaseName: getMainDatabaseName,
 };
