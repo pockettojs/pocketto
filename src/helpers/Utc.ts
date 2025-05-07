a3a381390c9d86fd1c5a58fa6370ba1dc8aedf5f
Kent Ng <kent.ng201@gmail.com>
2025-05-07 17:50:00 +0800

    fix utc bug now did not add the timezone offset

--- src/helpers/Utc.ts
+++ src/helpers/Utc.ts
@@ -13,13 +13,20 @@ export class Utc {
     }
 
     now(): string {
-        const moment = require('moment');
         const now = moment.utc().format('YYYY-MM-DDTHH:mm:ss');
-        const utcDate = moment(now).utcOffset(this.toUtc);
+        const utcDate = moment(now).add(this.toUtc, 'h').utcOffset(this.toUtc);
         return utcDate.format('YYYY-MM-DDTHH:mm:ssZ');
     }
 
+    /**
+     * Converts a UTC time to the local time zone.
+     * @param time the time format must be YYYY-MM-DDTHH:mm:ssZ
+     * @returns 
+     */
     from(time: string): string {
+        if (!Utc.isTimestampWithUtc(time)) {
+            throw new Error('Invalid time format. Expected format: YYYY-MM-DDTHH:mm:ssZ');
+        }
         const utcDate = moment(time).utcOffset(this.toUtc);
         return utcDate.format('YYYY-MM-DDTHH:mm:ssZ');
     }
